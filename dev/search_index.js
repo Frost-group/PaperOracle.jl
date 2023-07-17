aa1e4fea3ccffb78ecd1d1c66b974900237312ac
Documenter.jl <documenter@juliadocs.github.io>
2023-07-17 13:17:37 +0000

    build based on 2c41b65

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PaperOracle","category":"page"},{"location":"#PaperOracle","page":"Home","title":"PaperOracle","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PaperOracle.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PaperOracle]","category":"page"}]
+}
